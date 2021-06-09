5a1d1992f0fdf95f6ec8aca5fac099c04d4d8365
JakaPresecnik <jaka.presecnik@gmail.com>
2021-06-09 10:30:32 +0200

    BASIC tests done

--- backend/Readme.md
+++ backend/Readme.md
@@ -1,4 +1,25 @@
+
+Start virual enviroment:
+```
+py -3.7 -m venv venv
+venv\Scripts\activate
+```
+
+Set enviroment variables:
+```
 $env:DB_USER = "postgres"
 $env:DB_PASSWORD = "piko"
+```
+
+Run backend:
+```
+python app.py
+```
+
+Run tests:
+```
+dropdb -U postgres casting_agency_test
+python test_app.py -v
+```
 
 Scale up the minimap to 2 in VSCode for comment headlines to take effect.
--- backend/test_app.py
+++ backend/test_app.py
@@ -8,14 +8,15 @@ from sqlalchemy_utils.functions import database_exists, create_database
 from app import create_app
 from models import *
 
+
 class CastingAgencyTestCase(unittest.TestCase):
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#    _____       _     ___      _             
-#   |_   _|__ __| |_  / __| ___| |_ _  _ _ __ 
-#     | |/ -_|_-<  _| \__ \/ -_)  _| || | '_ \
-#     |_|\___/__/\__| |___/\___|\__|\_,_| .__/
-#                                       |_|
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    #    _____       _     ___      _
+    #   |_   _|__ __| |_  / __| ___| |_ _  _ _ __
+    #     | |/ -_|_-<  _| \__ \/ -_)  _| || | '_ \
+    #     |_|\___/__/\__| |___/\___|\__|\_,_| .__/
+    #                                       |_|
 
     def setUp(self):
         self.app = create_app()
@@ -31,7 +32,7 @@ class CastingAgencyTestCase(unittest.TestCase):
             self.db_host,
             self.db_name
         )
-        
+
         setup_db(self.app, self.database_path)
 
         # Initiate the database if not existant
@@ -55,105 +56,31 @@ class CastingAgencyTestCase(unittest.TestCase):
             self.actor.insert()
             self.movie.insert()
 
+            self.actor = Actor(
+                name='Actor 2',
+                age=32,
+                gender='Test'
+            )
+            self.movie = Movie(
+                title="Initial Movie 2",
+                release_date=datetime.date(2010, 5, 1)
+            )
+            self.actor.insert()
+            self.movie.insert()
+
     def tearDown(self):
         pass
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#    _____       _      
+#    _____       _
 #   |_   _|__ __| |_ ___
 #     | |/ -_|_-<  _(_-<
 #     |_|\___/__/\__/__/
 #
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                    __             __                 __          __    
-#     ___  ___  ___ / /_  ___ _____/ /____  _______   / /____ ___ / /____
-#    / _ \/ _ \(_-</ __/ / _ `/ __/ __/ _ \/ __(_-<  / __/ -_|_-</ __(_-<
-#   / .__/\___/___/\__/  \_,_/\__/\__/\___/_/ /___/  \__/\__/___/\__/___/
-#  /_/
-
-    # def test_post_actor(self):
-    #     test_actor = {
-    #         "name": "Mr. Test",
-    #         "age": "25",
-    #         "gender": "Unknown"
-    #     }
-    #     res = self.client().post('/actors', json=test_actor)
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
-    #     self.assertEqual(data['new_actor']['name'], test_actor['name'])
-    
-    # def test_422_post_actor_no_body(self):
-    #     res = self.client().post('/actors', json={})
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 422)
-    #     self.assertEqual(data['success'], False)
-    #     self.assertEqual(data['error'], 422)
-    
-    # def test_400_post_actor_actor_data_not_included(self):
-    #     res = self.client().post('/actors', json={'wrong': 'data'})
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 400)
-    #     self.assertEqual(data['success'], False)
-    #     self.assertEqual(data['error'], 400)
-    
-    # def test_409_post_actor_actor_already_enlisted(self):
-    #     test_actor = {
-    #          "name": "Initial Actor",
-    #          "age": "25",
-    #          "gender": "Unknown"
-    #      }
-    #     res = self.client().post('/actors', json=test_actor)
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 409)
-    #     self.assertEqual(data['success'], False)
-    #     self.assertEqual(data['error'], 409)
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                    __                   _           __          __    
-#     ___  ___  ___ / /_  __ _  ___ _  __(_)__ ___   / /____ ___ / /____
-#    / _ \/ _ \(_-</ __/ /  ' \/ _ \ |/ / / -_|_-<  / __/ -_|_-</ __(_-<
-#   / .__/\___/___/\__/ /_/_/_/\___/___/_/\__/___/  \__/\__/___/\__/___/
-#  /_/
-
-    # def test_post_movies(self):
-    #     test_movie = {
-    #         "title": "Tester",
-    #         "release_date":  "2022-02-02",
-    #     }
-    #     res = self.client().post('/movies', json=test_movie)
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
-    #     self.assertEqual(data['new_movie']['title'], test_movie['title'])
-
-    # def test_422_post_movie_no_body(self):
-    #     res = self.client().post('/movies', json={})
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 422)
-    #     self.assertEqual(data['success'], False)
-    #     self.assertEqual(data['error'], 422)
-
-    # def test_400_post_movie_movie_data_not_included(self):
-    #     res = self.client().post('/movies', json={'wrong': 'data'})
-    #     data = json.loads(res.data)
-
-    #     self.assertEqual(res.status_code, 400)
-    #     self.assertEqual(data['success'], False)
-    #     self.assertEqual(data['error'], 400)
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#              __             __                 __          __    
+#              __             __                 __          __
 #    ___ ____ / /_  ___ _____/ /____  _______   / /____ ___ / /____
 #   / _ `/ -_) __/ / _ `/ __/ __/ _ \/ __(_-<  / __/ -_|_-</ __(_-<
 #   \_, /\__/\__/  \_,_/\__/\__/\___/_/ /___/  \__/\__/___/\__/___/
@@ -177,7 +104,7 @@ class CastingAgencyTestCase(unittest.TestCase):
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#              __                   _           __          __    
+#              __                   _           __          __
 #    ___ ____ / /_  __ _  ___ _  __(_)__ ___   / /____ ___ / /____
 #   / _ `/ -_) __/ /  ' \/ _ \ |/ / / -_|_-<  / __/ -_|_-</ __(_-<
 #   \_, /\__/\__/ /_/_/_/\___/___/_/\__/___/  \__/\__/___/\__/___/
@@ -201,62 +128,239 @@ class CastingAgencyTestCase(unittest.TestCase):
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#       __    __    __                 __                 __          __    
+#       __    __    __                 __                 __          __
 #   ___/ /__ / /__ / /____   ___ _____/ /____  _______   / /____ ___ / /____
 #  / _  / -_) / -_) __/ -_) / _ `/ __/ __/ _ \/ __(_-<  / __/ -_|_-</ __(_-<
 #  \_,_/\__/_/\__/\__/\__/  \_,_/\__/\__/\___/_/ /___/  \__/\__/___/\__/___/
 #
 
-    # def test_delete_actor(self):
-    #     res = self.client().delete('/actors/1')
-    #     data = json.loads(res.data)
+    def test_delete_actor(self):
+        res = self.client().delete('/actors/1')
+        data = json.loads(res.data)
 
-    #     actor = Actor.query.filter_by(id=1).one_or_none()
+        actor = Actor.query.filter_by(id=1).one_or_none()
 
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
-    #     self.assertEqual(actor, None)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(actor, None)
 
-    # def test_404_delete_actor_not_exist(self):
-    #     res = self.client().delete('/actors/1000')
-    #     data = json.loads(res.data)
+    def test_404_delete_actor_not_exist(self):
+        res = self.client().delete('/actors/1000')
+        data = json.loads(res.data)
 
-    #     self.assertEqual(res.status_code, 404)
-    #     self.assertEqual(data['success'], False)
-    #     self.assertEqual(data['error'], 404)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#       __    __    __                       _           __          __    
+#       __    __    __                       _           __          __
 #   ___/ /__ / /__ / /____   __ _  ___ _  __(_)__ ___   / /____ ___ / /____
 #  / _  / -_) / -_) __/ -_) /  ' \/ _ \ |/ / / -_|_-<  / __/ -_|_-</ __(_-<
 #  \_,_/\__/_/\__/\__/\__/ /_/_/_/\___/___/_/\__/___/  \__/\__/___/\__/___/
 #
 
-# def test_delete_movies(self):
-#     res = self.client().delete('/movies/1')
-#     data = json.loads(res.data)
+    def test_delete_movies(self):
+        res = self.client().delete('/movies/1')
+        data = json.loads(res.data)
 
-#     movie = Movie.query.filter_by(id=1).one_or_none()
+        movie = Movie.query.filter_by(id=1).one_or_none()
 
-#     self.assertEqual(res.status_code, 200)
-#     self.assertEqual(data['success'], True)
-#     self.assertEqual(movie, None)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(movie, None)
+
+    def test_404_delete_movie_not_exist(self):
+        res = self.client().delete('/actors/1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                __      __              __                 __          __    
+#                    __             __                 __          __
+#     ___  ___  ___ / /_  ___ _____/ /____  _______   / /____ ___ / /____
+#    / _ \/ _ \(_-</ __/ / _ `/ __/ __/ _ \/ __(_-<  / __/ -_|_-</ __(_-<
+#   / .__/\___/___/\__/  \_,_/\__/\__/\___/_/ /___/  \__/\__/___/\__/___/
+#  /_/
+
+    def test_post_actor(self):
+        test_actor = {
+            "name": "Mr. Test",
+            "age": "25",
+            "gender": "Unknown"
+        }
+        res = self.client().post('/actors', json=test_actor)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['new_actor']['name'], test_actor['name'])
+
+    def test_422_post_actor_no_body(self):
+        res = self.client().post('/actors', json={})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 422)
+
+    def test_400_post_actor_actor_data_not_included(self):
+        res = self.client().post('/actors', json={'wrong': 'data'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 400)
+
+    def test_409_post_actor_actor_already_enlisted(self):
+        test_actor = {
+             "name": "Initial Actor",
+             "age": "25",
+             "gender": "Unknown"
+         }
+        res = self.client().post('/actors', json=test_actor)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 409)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 409)
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                    __                   _           __          __
+#     ___  ___  ___ / /_  __ _  ___ _  __(_)__ ___   / /____ ___ / /____
+#    / _ \/ _ \(_-</ __/ /  ' \/ _ \ |/ / / -_|_-<  / __/ -_|_-</ __(_-<
+#   / .__/\___/___/\__/ /_/_/_/\___/___/_/\__/___/  \__/\__/___/\__/___/
+#  /_/
+
+    def test_post_movies(self):
+        test_movie = {
+            "title": "Tester",
+            "release_date":  "2022-02-02",
+        }
+        res = self.client().post('/movies', json=test_movie)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['new_movie']['title'], test_movie['title'])
+
+    def test_422_post_movie_no_body(self):
+        res = self.client().post('/movies', json={})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 422)
+
+    def test_400_post_movie_movie_data_not_included(self):
+        res = self.client().post('/movies', json={'wrong': 'data'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 400)
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                __      __              __                 __          __
 #     ___  ___ _/ /_____/ /    ___ _____/ /____  _______   / /____ ___ / /____
 #    / _ \/ _ `/ __/ __/ _ \  / _ `/ __/ __/ _ \/ __(_-<  / __/ -_|_-</ __(_-<
 #   / .__/\_,_/\__/\__/_//_/  \_,_/\__/\__/\___/_/ /___/  \__/\__/___/\__/___/
 #  /_/
 
+    def test_patch_actors(self):
+        res = self.client().patch('/actors/2', json={'age': '40'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['updated_actor']['age'], 40)
+
+    def test_404_patch_actors_not_exist(self):
+        res = self.client().patch('/actors/1000', json={'age': '41'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+
+    def test_422_post_actors_no_body(self):
+        res = self.client().patch('/actors/2', json={})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 422)
+
+    def test_400_patch_actor_actor_data_not_included(self):
+        res = self.client().patch('/actors/2', json={'wrong': 'data'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 400)
+
+    def test_409_post_actor_actor_already_enlisted(self):
+        res = self.client().patch('/actors/2', json={"name": "Actor 2"})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 409)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 409)
+
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                __      __                    _           __          __    
+#                __      __                    _           __          __
 #     ___  ___ _/ /_____/ /    __ _  ___ _  __(_)__ ___   / /____ ___ / /____
 #    / _ \/ _ `/ __/ __/ _ \  /  ' \/ _ \ |/ / / -_|_-<  / __/ -_|_-</ __(_-<
 #   / .__/\_,_/\__/\__/_//_/ /_/_/_/\___/___/_/\__/___/  \__/\__/___/\__/___/
 #  /_/
 
+    def test_patch_movies(self):
+        res = self.client().patch('/movies/2', json={'title': 'New Title'})
+        data = json.loads(res.data)
+
+        movie = Movie.query.filter_by(id=2).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['updated_movie']['title'], 'New Title')
+
+    def test_422_patch_movies_no_body(self):
+        res = self.client().patch('/movies/2', json={})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 422)
+
+    def test_404_patch_movie_not_exist(self):
+        res = self.client().patch('/movies/1000', json={'title': 'Not Ex'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+
+    def test_400_patch_movie_movie_data_not_included(self):
+        res = self.client().patch('/movies/2', json={'wrong': 'data'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 400)
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#    ___                 _         _____       _
+#   | __|_ _____ __ _  _| |_ ___  |_   _|__ __| |_
+#   | _|\ \ / -_) _| || |  _/ -_)   | |/ -_|_-<  _|
+#   |___/_\_\___\__|\_,_|\__\___|   |_|\___/__/\__|
+#
 
 if __name__ == "__main__":
     unittest.main()
